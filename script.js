06cfb2d2fb13155873d9d9126b261f97527528aa
mikeyyj <mikejerez@gmail.com>
2024-02-06 18:11:32 -0500

    added main content

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleAnswer(question) {
+    var answer = question.nextElementSibling;
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+    } else {
+        answer.style.display = "block";
+    }
+}
